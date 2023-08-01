36ecfa510e6b99f45a0d4e3e2301dbcb8050925e
rasulcando <54900312+rasulcando@users.noreply.github.com>
2023-08-01 23:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
+import sqlite3, datetime
 
 app = Flask(__name__)
-
+app.config['JWT_SECRET_KEY'] = 'IzraRasul@300991'
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(hours=2)
+jwt = JWTManager(app)
 # Create a SQLite database connection
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
@@ -20,19 +23,23 @@ c.execute('''
 conn.commit()
 conn.close()
 
+
 # Helper function to convert database rows to dictionaries
 def row_to_dict(row):
     return {"name": row[0], "email": row[1], "password": row[2]}
 
+
 # API endpoint to list all users
 @app.route('/users/list', methods=['GET'])
+@jwt_required()
 def list_users():
-  with sqlite3.connect("users.db") as conn:
-    c = conn.cursor()
-    c.execute('SELECT * FROM users')
-    users = [row_to_dict(row) for row in c.fetchall()]
-    return jsonify(users)
-    conn.close()
+    with sqlite3.connect("users.db") as conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM users')
+        users = [row_to_dict(row) for row in c.fetchall()]
+        return jsonify(users)
+        conn.close()
+
 
 # Helper function to insert a new user into the database
 def insert_user(name, email, password):
@@ -42,43 +49,51 @@ def insert_user(name, email, password):
     conn.commit()
     conn.close()
 
+
 # API endpoint to create a new user
 @app.route('/users/create', methods=['POST'])
 def create_user():
-  # with sqlite3.connect("users.db") as conn:
-  #   c = conn.cursor()
     data = request.get_json()
     if not data or "name" not in data or "email" not in data or "password" not in data:
         return jsonify({"message": "Invalid request data"}), 400
-
-    name = data["name"]
     email = data["email"]
+    user_data = get_user_by_email(email)
+    if user_data:
+        return jsonify({"message": "User Already Exists"}), 404
+    name = data["name"]
     password = generate_password_hash(data["password"])  # Hash the password before storing it
-
     insert_user(name, email, password)
     return jsonify({"message": "User registered successfully"}), 201
 
-# API endpoint to update a user by username
-@app.route('/users/<user>', methods=['PUT'])
-def update_user(user):
-  with sqlite3.connect("users.db") as conn:
-    c = conn.cursor()
-    data = request.get_json()
-    if not data or "name" not in data or "email" not in data:
-        return jsonify({"message": "Invalid request data"}), 400
 
-    c.execute('UPDATE users SET name=?, email=? WHERE name=?', (data["name"], data["email"], user))
-    conn.commit()
-    return jsonify({"message": "User updated successfully"}), 200
+# API endpoint to update a user by username
+@app.route('/users/update', methods=['PUT'])
+@jwt_required()
+def update_user():
+    with sqlite3.connect("users.db") as conn:
+        c = conn.cursor()
+        data = request.get_json()
+        if not data or "name" not in data or "email" not in data:
+            return jsonify({"message": "Invalid request data"}), 400
+
+        c.execute('UPDATE users SET name=?, email=? WHERE email=?', (data["name"], data["email"], data["email"]))
+        conn.commit()
+        return jsonify({"message": "User updated successfully"}), 200
+
 
 # API endpoint to delete a user by username
-@app.route('/users/<user>', methods=['DELETE'])
-def delete_user(user):
-  with sqlite3.connect("users.db") as conn:
-    c = conn.cursor()
-    c.execute('DELETE FROM users WHERE name=?', (user,))
-    conn.commit()
-    return jsonify({"message": "User deleted successfully"}), 200
+@app.route('/user/delete', methods=['DELETE'])
+@jwt_required()
+def delete_user():
+    with sqlite3.connect("users.db") as conn:
+        c = conn.cursor()
+        data = request.get_json()
+        if not data or "email" not in data:
+            return jsonify({"message": "Invalid request data"}), 400
+        c.execute('DELETE FROM users WHERE email=?', data["email"])
+        conn.commit()
+        return jsonify({"message": "User deleted successfully"}), 200
+
 
 # # API endpoint to check login by username
 def get_user_by_email(email):
@@ -89,11 +104,12 @@ def get_user_by_email(email):
     conn.close()
     return user_data
 
+
 # Route for user login
 @app.route('/login', methods=['POST'])
 def login_user():
     data = request.get_json()
-    if not data or "name" not in data or "email" not in data or "password" not in data:
+    if not data or "email" not in data or "password" not in data:
         return jsonify({"message": "Invalid request data"}), 400
 
     email = data["email"]
@@ -109,7 +125,9 @@ def login_user():
 
     # Check if the entered password matches the hashed password
     if check_password_hash(hashed_password, password):
-        return jsonify({"message": "Login successful"}), 200
+        # Generate a JWT token with the user's email as the identity
+        access_token = create_access_token(identity=email)
+        return jsonify({"access_token": access_token}), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
 
@@ -127,8 +145,10 @@ def update_user_password(email, new_password):
     conn.commit()
     conn.close()
 
+
 # Route for changing user password
 @app.route('/changepassword', methods=['POST'])
+@jwt_required()
 def change_password():
     data = request.get_json()
     if not data or "email" not in data or "old_password" not in data or "new_password" not in data:
@@ -154,8 +174,7 @@ def change_password():
     update_user_password(email, new_password)
 
     return jsonify({"message": "Password updated successfully"}), 200
-    
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
