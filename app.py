be8a5efc6f4c5ff69f0aea0841b63e20c95b5c61
rasulcando <54900312+rasulcando@users.noreply.github.com>
2023-08-01 17:08:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,161 @@
+# app.py
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+
+app = Flask(__name__)
+
+# Create a SQLite database connection
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+# Create a users table if it doesn't exist
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL 
+    )
+''')
+conn.commit()
+conn.close()
+
+# Helper function to convert database rows to dictionaries
+def row_to_dict(row):
+    return {"name": row[0], "email": row[1], "password": row[2]}
+
+# API endpoint to list all users
+@app.route('/users/list', methods=['GET'])
+def list_users():
+  with sqlite3.connect("users.db") as conn:
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    users = [row_to_dict(row) for row in c.fetchall()]
+    return jsonify(users)
+    conn.close()
+
+# Helper function to insert a new user into the database
+def insert_user(name, email, password):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', (name, email, password))
+    conn.commit()
+    conn.close()
+
+# API endpoint to create a new user
+@app.route('/users/create', methods=['POST'])
+def create_user():
+  # with sqlite3.connect("users.db") as conn:
+  #   c = conn.cursor()
+    data = request.get_json()
+    if not data or "name" not in data or "email" not in data or "password" not in data:
+        return jsonify({"message": "Invalid request data"}), 400
+
+    name = data["name"]
+    email = data["email"]
+    password = generate_password_hash(data["password"])  # Hash the password before storing it
+
+    insert_user(name, email, password)
+    return jsonify({"message": "User registered successfully"}), 201
+
+# API endpoint to update a user by username
+@app.route('/users/<user>', methods=['PUT'])
+def update_user(user):
+  with sqlite3.connect("users.db") as conn:
+    c = conn.cursor()
+    data = request.get_json()
+    if not data or "name" not in data or "email" not in data:
+        return jsonify({"message": "Invalid request data"}), 400
+
+    c.execute('UPDATE users SET name=?, email=? WHERE name=?', (data["name"], data["email"], user))
+    conn.commit()
+    return jsonify({"message": "User updated successfully"}), 200
+
+# API endpoint to delete a user by username
+@app.route('/users/<user>', methods=['DELETE'])
+def delete_user(user):
+  with sqlite3.connect("users.db") as conn:
+    c = conn.cursor()
+    c.execute('DELETE FROM users WHERE name=?', (user,))
+    conn.commit()
+    return jsonify({"message": "User deleted successfully"}), 200
+
+# # API endpoint to check login by username
+def get_user_by_email(email):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM users WHERE email=?', (email,))
+    user_data = c.fetchone()
+    conn.close()
+    return user_data
+
+# Route for user login
+@app.route('/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    if not data or "name" not in data or "email" not in data or "password" not in data:
+        return jsonify({"message": "Invalid request data"}), 400
+
+    email = data["email"]
+    password = data["password"]
+
+    # Fetch the user by email from the database
+    user_data = get_user_by_email(email)
+    if not user_data:
+        return jsonify({"message": "User not found"}), 404
+
+    # Get the hashed password from the database
+    hashed_password = user_data[2]
+
+    # Check if the entered password matches the hashed password
+    if check_password_hash(hashed_password, password):
+        return jsonify({"message": "Login successful"}), 200
+    else:
+        return jsonify({"message": "Invalid credentials"}), 401
+
+
+# Helper function to update the user's password in the database
+def update_user_password(email, new_password):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    # Generate the hashed password
+    hashed_password = generate_password_hash(new_password)
+
+    # Update the user's password in the database
+    c.execute('UPDATE users SET password=? WHERE email=?', (hashed_password, email))
+    conn.commit()
+    conn.close()
+
+# Route for changing user password
+@app.route('/changepassword', methods=['POST'])
+def change_password():
+    data = request.get_json()
+    if not data or "email" not in data or "old_password" not in data or "new_password" not in data:
+        return jsonify({"message": "Invalid request data"}), 400
+
+    email = data["email"]
+    old_password = data["old_password"]
+    new_password = data["new_password"]
+
+    # Fetch the user by email from the database
+    user_data = get_user_by_email(email)
+    if not user_data:
+        return jsonify({"message": "User not found"}), 404
+
+    # Get the hashed password from the database
+    hashed_password = user_data[2]
+
+    # Check if the entered old password matches the stored hashed password
+    if not check_password_hash(hashed_password, old_password):
+        return jsonify({"message": "Invalid old password"}), 401
+
+    # Update the user's password with the new hashed password
+    update_user_password(email, new_password)
+
+    return jsonify({"message": "Password updated successfully"}), 200
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
