07321e43645b282e26dd1056832808959995e6fd
rasulcando <54900312+rasulcando@users.noreply.github.com>
2023-08-02 16:29:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    DEBUG = True
+    JWT_SECRET_KEY = 'xxxxxxxxx'
+    JWT_EXPIRATION_DELTA = 'datetime.timedelta(hours=2)'
+    MYSQL_HOST = 'xxxxxxxx'
+    MYSQL_USER = 'xxxxxxxx'
+    MYSQL_PASSWORD = 'xxxxxxxx'
+    MYSQL_DB = 'xxxxxxx'
